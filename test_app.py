cc367055417291a8b691c41b15e64ce24e3b85c7
2dollar2dollar <vladislav.grechushkin.666@gmail.com>
2026-03-27 16:11:48 +0300

    Initial commit: Simple Python Flask app

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import unittest, app, json
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+    def test_hello_endpoint(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+    def test_health_endpoint(self):
+        response = self.app.get('/health')
+        self.assertEqual(response.status_code, 200)
+if __name__ == '__main__':
+    unittest.main()
